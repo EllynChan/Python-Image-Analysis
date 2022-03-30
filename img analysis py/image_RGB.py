27354cd69e080e796899e70ba81abb417ba2076f
EllynChan <eve9323@163.com>
2022-03-29 18:27:19 -0700

    added contrast

--- img analysis py/image_RGB.py
+++ img analysis py/image_RGB.py
@@ -8,34 +8,31 @@ from tabulate import tabulate
 import os
 
 #set directory (change to whatever your directory is)
-dst_img = "C:\\Users\\Ellyn\\Desktop\\img analysis py\\Images"
+#dst_img = "C:\\Users\\Ellyn\\Desktop\\img analysis py\\Images"
+dst_img = "C:\\Users\\Palombo Lab\\Desktop\\img analysis\\img analysis py\\Images"
 
 #listing files in images folder
 list_img = os.listdir(dst_img)
 
 #iterating over dst_image to get the images as arrays
 for image in sorted(list_img):
+    
     [file_name, ext] = os.path.splitext(image) #splitting file name from its extension
     arr = np.array(Image.open(os.path.join(dst_img, image))) #creating arrays for all the images
-    [h, w] = np.shape(arr)[0:2]#calculating height and width for each image
     arr_dim = arr.ndim #calculating the dimension for each array
-    arr_shape = arr.shape #calculating the shape for each array
+
     if arr_dim == 2:
         arr_mean = np.mean(arr)
         print(f'[{file_name}, greyscale={arr_mean:.1f}]')
     else:
         arr_mean = np.mean(arr, axis=(0,1))
-        if len(arr_mean) == 3: #RGB CASE
-            R = arr_mean[0]
-            G = arr_mean[1]
-            B = arr_mean[2]
+        R = arr_mean[0]
+        G = arr_mean[1]
+        B = arr_mean[2]
+        newArr = np.array(Image.open(os.path.join(dst_img, image)).convert('L'))
 
-            #LUMI formula: https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
-            print(f'[{file_name}, R={R:.1f},  G={G:.1f}, B={B:.1f}, LUMI={(0.2126*R + 0.7152*G + 0.0722*B):.1f} ]')
-        else: #ALPHA CASE
-            print(f'[{file_name}, R={R:.1f}, G={G:.1f}, B={B:.1f}, ALPHA={arr_mean[3]:.1f}]')
-
-
-
+        #LUMI 0-255, higher means more intense formula: https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+        #CONT 0-255, higher means greater contrast in luminance of the img's grayscale
+        print(f'[{file_name}, R={R:.1f},  G={G:.1f}, B={B:.1f}, LUMI={(0.2126*R + 0.7152*G + 0.0722*B):.1f}, CONT={np.max(newArr) - np.min(newArr)}]')
 
 #[for insta should be: R=234.1,  G=153.1, B=186.2 ]
